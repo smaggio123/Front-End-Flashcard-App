333f3a1f4e4b551a6533aa46da39e519c528a625
smaggio123 <89543842+smaggio123@users.noreply.github.com>
2023-07-02 11:55:39 -0400

    Implemented shuffling the terms/definitions

--- src/components/Written.js
+++ src/components/Written.js
@@ -1,14 +1,15 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import "./Written.css";
-import { React, useState } from 'react';
+import { React, useState} from 'react';
 import { useLocation,useNavigate } from 'react-router-dom';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 function Written(){
     const [hintVisible, setHintVisible] = useState(false);
-    const setList = [["word1","def1"],["word2","def2"],["word3","def3"],["word4","def4"],["word5","def5"]];
+    const originalList = [["word1","def1"],["word2","def2"],["word3","def3"],["word4","def4"],["word5","def5"]];
+    const [setList,setSetList]=useState([["word1","def1"],["word2","def2"],["word3","def3"],["word4","def4"],["word5","def5"]]);
     const [currentTerm, setCurrentTerm] = useState(setList[0][0]);
     const [currentDef, setCurrentDef] = useState(setList[0][1]);
     const [cardIndex, setCardIndex] = useState(0);
@@ -21,16 +22,38 @@ function Written(){
     const searchParams = new URLSearchParams(location.search);
     const selectedData = searchParams.get('data');
 
+    // Fisher-Yates shuffle algorithm
+    //Shuffles the list of terms and definitions
+    const shuffleArray = (arr) => {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+        return arr;
+    };
 
-    const handleBtnClick = () =>{
+    //Handles shuffling the set
+    const handleShuffle=()=>{
+        setwritingInputShuffleSet(!writingInputShuffleSet);
+        if (!writingInputShuffleSet) {
+            setSetList(shuffleArray([...setList]));
+        } else {
+            setSetList(originalList);
+        }
+    }
+
+    //Handles the hint button being clicked
+    const handleHintBtn = () =>{
         let l = hintVisible;
         setHintVisible(!l)
     }
+    //Sends user back to learn page
     const handleBackTolearn = () =>{
         const { pathname } = location;
         const newUrl = `${pathname.substring(0, pathname.lastIndexOf('/'))}?data=${encodeURIComponent(selectedData)}`;
         navigate(newUrl)
     }
+    //Brings up the settings page
     const handlesettingsClicked = () =>{
         setSettingsActive(!settingsActive);
     }
@@ -41,6 +64,7 @@ function Written(){
         setSettingsActive(!settingsActive);
     }
     */
+   //Checks if the user got the term/def correct
     const handleSubmit = () =>{
         setHintVisible(false);
         if(answerWithDefinition){
@@ -60,6 +84,7 @@ function Written(){
         setCurrentDef(setList[c][1])
         setWrittenInput("");
     }
+    //Allows the user to move on without guessing the term/def
     const handleIDKBro = () =>{
         setHintVisible(false);
         if(answerWithDefinition)alert("The answer is: "+ currentDef)
@@ -83,8 +108,6 @@ function Written(){
                 <div id="writtenSettingsContents">
                     {/* 
                         Options:
-                        -Starting term
-                        -Number of terms
                         -Swapping terms with definitions
                         -shuffle terms
 
@@ -95,7 +118,7 @@ function Written(){
                     </div>
                     <div className="writtenSettingAttributes">
                         <h3 id="writtenSettingShuffle">Shuffle set</h3>
-                        <input type="checkbox" onChange={()=>setwritingInputShuffleSet(!writingInputShuffleSet)}/>
+                        <input type="checkbox" checked={writingInputShuffleSet} onChange={()=>handleShuffle()}/>
                     </div>
                         {/* <button id="writtenUpdateBtn" onClick={()=>handleUpdateBtn()}>Update</button> */}
                     </div>
@@ -109,7 +132,7 @@ function Written(){
                 <div id="writtenHintDiv">
                     <h2 id={hintVisible?"writtenHintShown":"writtenHintHidden"}>{answerWithDefinition?currentDef:currentTerm}</h2>
                 
-                    <button id="writtenHintButton" onClick={()=>handleBtnClick()}>Hint</button>
+                    <button id="writtenHintButton" onClick={()=>handleHintBtn()}>Hint</button>
                 </div>
                 <div>
                     <input id="writtenInput" type="text" value={writtenInput} onChange={(e)=>setWrittenInput(e.target.value)}/>
