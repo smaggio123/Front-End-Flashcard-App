815e1bd642d2d0b7e1e7176ad4af80a3ba04ba51
smaggio123 <smaggio126@gmail.com>
2023-08-18 17:35:54 -0400

    Fixed timer bug

--- src/components/CopyAreaPage/CopyArea.js
+++ src/components/CopyAreaPage/CopyArea.js
@@ -1,8 +1,12 @@
 import LearnBtn from "../LearnBtn";
 import "./CopyArea.css";
-import { React,useState, useEffect } from 'react';
+import { React,useState, useRef, useEffect } from 'react';
 
 function CopyArea(){
+    // We need ref in this, because we are dealing
+    // with JS setInterval to keep track of it and
+    // stop it when needed
+    const Ref = useRef(null);
     const setList=[
         ["word1","def1"],
         ["Diffusion","The passive movement of particles from an area of high concentration to low concentration"],
@@ -16,41 +20,45 @@ function CopyArea(){
     const [showDefinition,setShowDefinition] = useState(true);
     const [writtenInput, setWrittenInput] = useState("");
     const [copyAreaBorderColorWhite, setCopyAreaBorderColorWhite] = useState(true);
-    const [timer, setTimer] = useState(5);
-    
+    const [resetTime,setResetTime]=useState(3);
+    const [totalTime, setTotalTime] = useState(0);
+    const [operation,setOperation] = useState(-1);
+    const [canType, setCanType] = useState(true);
+    const listOfOperations = {
+	nothing: -1,
+    hideWord: 0,
+	showWord: 1,
+    goRight: 2,
+    }
+
     useEffect(() => {
-        if(writtenInput===currentDef){
-            setShowDefinition(true);
-            setCopyAreaBorderColorWhite(false);
-            setTimer(3);
-            const interval = setInterval(() => {
-                setTimer((prevTimer)=> {
-                    if (prevTimer === 0) {
-                        setShowDefinition(true);
-                        clearInterval(interval);
-                        return 5;
-                    }
-                    return prevTimer - 1;
-                    });
-                }, 1000);
+        if (Ref.current) clearInterval(Ref.current);
+        let id = null;
+        if(totalTime > 0)id = setTimeout(() => setTotalTime(totalTime - 1),1000)
+        else{
+            switch(operation){
+                case listOfOperations.nothing:
+                    break;
+                case listOfOperations.hideWord:
+                    //Hide word
+                    setShowDefinition(false)
+                    break;
+                case listOfOperations.showWord:
+                    //Show word
+                    setShowDefinition(true)
+                    break;
+                case listOfOperations.goRight:
+                    //Go right
+                    handleRightBtnClick();
+                    break;
+                default:
+                    //Don't do anything
+                    break;
+
+            }
         }
-        else if(writtenInput !== ''){
-            setShowDefinition(false);
-            const interval = setInterval(() => {
-            setTimer((prevTimer)=> {
-                if (prevTimer === 0) {
-                    clearInterval(interval);
-                    setShowDefinition(true);
-                    return 5;
-                }
-                return prevTimer - 1;
-                });
-            }, 1000);
-        
-            // Clean up the interval when the component unmounts
-            return () => clearInterval(interval);
-        }
-      }, [writtenInput]);
+        Ref.current = id;
+    }, [totalTime]);
 
     //Allows the user to move on without guessing the term/def
     const handleRightBtnClick = () =>{
@@ -61,7 +69,8 @@ function CopyArea(){
         setCurrentTerm(setList[c][0])
         setCurrentDef(setList[c][1])
         setWrittenInput("");
-        setTimer(5);
+        setCopyAreaBorderColorWhite(true)
+        setCanType(true)
     }
     const handleLeftBtnClick = () =>{
         let c = cardIndex;
@@ -75,29 +84,45 @@ function CopyArea(){
         setCurrentTerm(setList[c][0])
         setCurrentDef(setList[c][1])
         setWrittenInput("");
-        setTimer(5);
+        setCopyAreaBorderColorWhite(true)
+        setCanType(true)
     }
-    const copyAreaHandleTextInput = (val) =>{
-        setWrittenInput(val)
-        if(val===currentDef){
-            setTimeout(() => {
-                setCopyAreaBorderColorWhite(true)
-                handleRightBtnClick();
-              }, 3000); // Wait for 3 seconds
+     const copyAreaHandleTextInput = (val) => {
+        if(canType){
+            setWrittenInput(val)
+            setOperation(listOfOperations.showWord)
+            setShowDefinition(false)
+            if(val === ""){
+                setShowDefinition(true)
+                setTotalTime(0)
+            }
+            else{
+                setTotalTime(resetTime)
+            }
+            if(val.toLowerCase()===currentDef.toLowerCase()){
+                setCanType(false)
+                setCopyAreaBorderColorWhite(false)
+                setShowDefinition(true)
+                setOperation(listOfOperations.goRight)
+                setTotalTime(3)
+            }
             
         }
-        setShowDefinition(false)
-    }
-
+        }
+        
     return(
         <>
         <LearnBtn/>
+        <div id="CopyAreaTimerContainer">
+                <div id="CopyAreaTimerContainingDiv">
+                    <h2 id="CopyAreaTimerDisplayedTime">{totalTime}</h2>
+                </div>
+            </div>
         <div id="copyAreaContainingDiv">
             <h1 id="copyAreaTitleHeader">Copy Area</h1>
             <h2 id="copyAreaCurrentTerm">{currentTerm}</h2>
             <div id="copyAreaContainingShownWords" style={{border: copyAreaBorderColorWhite? "1px solid white": "1px solid green"}}>
                 <p id="copyAreaShowingDef" style={{visibility: showDefinition? "visible":"hidden"}}>{currentDef}</p>
-                <div id="copyAreaTimerArea"><h2 id="copyAreaTimer">{timer}</h2></div>
             </div>
             <div id="copyAreaInputArea">
                 <textarea id="copyAreaInput" value={writtenInput} onChange={(e)=>copyAreaHandleTextInput(e.target.value)}></textarea>
